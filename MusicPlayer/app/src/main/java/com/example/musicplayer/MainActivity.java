32be797c73d03b87b211006938b54bf721ab69e9
Rushi2800 <ram-amin@hotmail.com>
2021-11-25 17:11:39 -0500

    Music app commit

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer =MediaPlayer.create(this, R.raw.song);
+        Button Play = (Button) findViewById(R.id.Play);
+
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "The Song is Over", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        Button Pause = (Button) findViewById(R.id.Pause);
+
+        Pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+        Button Selectsong = (Button) findViewById(R.id.Select_song);
+        Selectsong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
