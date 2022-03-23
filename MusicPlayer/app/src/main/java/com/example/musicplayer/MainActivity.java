e91e2522532ff1f13922f04d80aaec3319751b76
Rushi2800 <ram-amin@hotmail.com>
2022-03-23 15:51:21 -0400

    logcat-update-3

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity implements Runnable{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MainActivity obj = new MainActivity();
-        Thread thread = new Thread(obj);
-        thread.start();
+//        MainActivity obj = new MainActivity();
+//        Thread thread = new Thread(obj);
+//        thread.start();
 
 
         mediaPlayer =MediaPlayer.create(this, R.raw.song);
@@ -64,17 +64,17 @@ public class MainActivity extends AppCompatActivity implements Runnable{
 
     }
 
-    @Override
+//    @Override
     public void run() {
+//
+//        while(true){
+//            Log.w(LOG_TAG, "Vehicle.brake.press()");
+//            try {
+//                TimeUnit.SECONDS.sleep(3);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
-        while(true){
-            Log.w(LOG_TAG, "Vehicle.brake.press()");
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
+     }
 }
